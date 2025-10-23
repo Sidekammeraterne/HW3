54df074e3bfb3b32099217b48ba39ff414356580
Marie Johansen <majoh@itu.dk>
2025-10-23 12:21:39 +0200

    Initial setup
    Co-authored-by: sarabagger <sidekammeraterne@saralouise.dk>
    Co-authored-by: CharlottePlanteig <charlotte.planteig@hotmail.com>

--- client/client.go
+++ client/client.go
@@ -1,1 +1,38 @@
-package client
+package main
+
+import (
+	proto "ITUServer/grpc"
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+)
+
+func main() {
+	// creates a connection related to a client who can speak to the specified port, grpc client can ask for a service
+	conn, err := grpc.NewClient("localhost:5050", grpc.WithTransportCredentials(insecure.NewCredentials())) //security, don't think much about it."Boilerpalte<"
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	client := proto.NewChitChatClient(conn) //creates a new client and opens for the connection
+
+	//students, err := client.GetStudents(context.Background(), &proto.Empty{}) //creates an array or slice with the repeated strings of stufents
+	//if err != nil {
+	//log.Fatalf("we did not recieve anything or failed to send %v", err)
+	//	}
+
+	//courses, err := client.GetCourses(context.Background(), &proto.Empty{})
+	//if err != nil {
+	//	log.Fatalf("we did not recieve anything or failed to send %v", err)
+	//}
+
+	//prints all the students
+	//for _, student := range students.Students {
+	//	log.Println(" - " + student)
+	//}
+
+	//prints all the courses
+	//for _, course := range courses.Courses {
+	//	log.Println(" - " + course)
+	//}
+}
--- server/server.go
+++ server/server.go
@@ -1,1 +1,52 @@
-package server
+package main
+
+import (
+	proto "ITUServer/grpc" //make connection
+	"context"              //make connection - the context of the connection
+	"google.golang.org/grpc"
+	"log" //logs - used to keep track of messages
+	"net" //make connection to net
+)
+
+type ChitChatServer struct {
+	//part of the proto - we are creating an Unimplemented server
+	proto.UnimplementedChitChatServer
+	//todo: define what we want the struct to contain, if it should contain anything
+}
+
+// Method only be activated if called through proto
+// (s*ITU_databaseServer) the reciever - it points to the struct ITU_databaseServer that we want to use (it is the same as parsing the student as a parameter)
+// GetStudents(ctx context.Context, in *proto.Empty) function name and parameters of the function
+// (*proto.Students, error) is the return type
+func (s *ChitChatServer) publishMessage(ctx context.Context, in *proto.Message) (*proto.Empty, error) { //if we want something with an empty in it
+	return &proto.Empty{}, nil //returns the pointer to the student in memory - what we are encourages to do [should be able to see this from the function declaration (?)]
+}
+
+func main() {
+	//creation of clients
+	server := &ChitChatServer{}
+	server.start_server()
+}
+
+func (s *ChitChatServer) start_server() {
+	grpcServer := grpc.NewServer()              //creates new gRPC server instance
+	listener, err := net.Listen("tcp", ":5050") //listens on TCP port using net.Listen
+	if err != nil {
+		log.Fatalf("Did not work")
+	}
+
+	proto.RegisterChitChatServer(grpcServer, s) //registers the server implementation with gRPC
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatalf("Did not work")
+	}
+
+}
+
+/* Everytime something is changed in this file, run the following command in the terminal:
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative proto.proto
+*/
